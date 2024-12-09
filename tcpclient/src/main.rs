bab63ee585433ba6ebaa5ef8afd084fdcaec60dd
Jorge Horacio Cué Cantú <jorge.cue@gmail.com>
2024-12-08 21:23:51 -0600

    Improving error handling

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,14 +1,21 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::process::exit;
 use std::str;
 
 fn main() {
     let mut stream = TcpStream::connect("localhost:3000").unwrap();
-    stream.write("Hello".as_bytes()).unwrap();
+    stream.write("Hello".as_bytes()).unwrap_or_else(|e| {
+        eprintln!("failed to write: {}", e);
+        exit(1);
+    });
     let mut buffer = [0; 5];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).unwrap_or_else(|e| {
+        eprintln!("failed to read: {}", e);
+        exit(1);
+    });
     println!(
         "Got response from server:{:?}",
-        str::from_utf8(&buffer).unwrap()
+        str::from_utf8(&buffer).unwrap();
     );
 }
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,13 +1,24 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::process::exit;
+
 fn main() {
     let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000");
     for stream in connection_listener.incoming() {
-        let mut stream = stream.unwrap();
+        let mut stream = stream.unwrap_or_else(move |e| {
+            eprintln!("Error: {}", e);
+            exit(1);
+        });
         println!("Connection established");
         let mut buffer = [0; 1024];
-        stream.read(&mut buffer).unwrap();
-        stream.write(&mut buffer).unwrap();
+        stream.read(&mut buffer).unwrap_or_else(|e| {
+            eprintln!("Error: {}", e);
+            exit(1);
+        });
+        stream.write(&mut buffer).unwrap_or_else(|e| {
+            eprintln!("Error: {}", e);
+            exit(1);
+        });
     }
 }
